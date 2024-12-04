b0746ff7d12735248866e92f145261a6c570bf01
ying <3012174688@qq.com>
2024-12-04 23:25:42 +0800

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 引入 Element Plus 样式
+import 'element-plus/dist/index.css'
+
+// 引入 Element Plus 组件库
+import ElementPlus from 'element-plus'
+
+const app = createApp(App)
+
+// 使用 Element Plus
+app.use(ElementPlus)
+
+app.mount('#app')
